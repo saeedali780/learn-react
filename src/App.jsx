1a2dd8a45cba49707e1318ac3eda051874f94ec3
Saeed Ali <saeedaliofficial77@gmail.com>
2025-06-22 04:20:36 +0500

    learning use reducer hook in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useReducer } from 'react';
 
 const App = () => {
+  const [state,disptach] = useReducer(reducer,0);
+  function reducer(state,action){
+    if (action.type == 'increament') {
+      return state + action.payload;
+    }
+    if (action.type == 'decreament') {
+      return state - action.payload;
+    }
+  }
   return (
-    <div>App</div>
+    <div>
+        <h1> {state}</h1>
+      <button onClick={()=> disptach({type: "increament", payload: 1})}>Increment Count</button>
+      <button onClick={()=> disptach({type: "decreament", payload: 1})}>Decreament Count</button>
+    </div>
   )
 }
 
