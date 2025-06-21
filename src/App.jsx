6b142ef790a4c950c48d6f9005615299951b565e
Saeed Ali <saeedaliofficial77@gmail.com>
2025-06-22 04:30:05 +0500

    Using Object As State In useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import React, {useReducer } from 'react';
 
 const App = () => {
-  const [state,disptach] = useReducer(reducer,0);
+  const [state,disptach] = useReducer(reducer,{count:0});
   function reducer(state,action){
     if (action.type == 'increament') {
-      return state + action.payload;
+    //  return state + action.payload;
+    return {...state,count: state.count + action.payload }
     }
     if (action.type == 'decreament') {
-      return state - action.payload;
+      return  {...state,count: state.count - action.payload } //state - action.payload;
     }
   }
   return (
     <div>
-        <h1> {state}</h1>
+        <h1> {state.count}</h1>
       <button onClick={()=> disptach({type: "increament", payload: 1})}>Increment Count</button>
       <button onClick={()=> disptach({type: "decreament", payload: 1})}>Decreament Count</button>
     </div>
