242348b889736f712c09c9b4d1df8b314e313382
Saeed Ali <saeedaliofficial77@gmail.com>
2025-06-22 04:58:32 +0500

    Understanding Reducer Using Bank Account Example

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
-import React, {act, useReducer } from 'react';
+import { useState, useReducer } from 'react';
 
 const App = () => {
-  const [state,disptach] = useReducer(reducer,{count: 0, inreamentByValue: 1, decreamentByValue:1 });
+  const [state,dispatch] = useReducer(reducer,{balance: 0});
+  const [amount,setAmount] = useState(0)
   function reducer(state,action){
-    if (action.type == 'increament') {
-    //  return state + action.payload;
-    return {...state,count: state.count + action.payload }
+    if(action.type == 'add'){
+      return {...state,balance:state.balance + action.payload}
     }
-    if (action.type == 'decreament') {
-      return  {...state,count: state.count - action.payload } //state - action.payload;
-    }
-    if(action.type === "setIncreament"){
-      return {...state, inreamentByValue: action.payload}
-    }
-    if(action.type === "setDecreament"){
-      return {...state, decreamentByValue: action.payload}
+    if(action.type == 'withdraw'){
+      return {...state, balance: state.balance - action.payload}
     }
   }
+
   return (
+
     <div>
-        <h1> {state.count}</h1>
-        <label>Increament By:</label>
-        <input type="text" value={state.inreamentByValue} onChange={(e)=> disptach({type: "setIncreament", payload: Number(e.target.value)})} />
-        <br /><br />
-        <label>Decreament By: </label>
-        <input type="text" value={state.decreamentByValue} onChange={(e)=> disptach({type: "setDecreament", payload: Number(e.target.value)})} />
-        <br />
-      <button onClick={()=> disptach({type: "increament", payload: state.inreamentByValue})}>Increment Count</button>
-      <button onClick={()=> disptach({type: "decreament", payload: state.decreamentByValue})}>Decreament Count</button>
+      {state.balance}
+
+      <input type="text" value={amount} onChange={(e)=> setAmount(e.target.value)}/>
+      <button onClick={()=>dispatch({type:"add",payload: Number(amount)})}>deposit</button>
+      <button onClick={()=>dispatch({type:"withdraw",payload: Number(amount)})}>withdraw</button>
     </div>
   )
 }
