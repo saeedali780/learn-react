2bb12941910d2cca1a50f1b60f1aca4c5e7348ed
Saeed Ali <saeedaliofficial77@gmail.com>
2025-06-30 18:17:48 +0500

    programmatic navigate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 
 const Home = () => {
-  
-const navigate = useNavigate()
+const [username,setUserName] = useState("");  
+const navigate = useNavigate();
+
+
   const handleClick = ()=>{
-    navigate('/dashboard/profile')
+    navigate('/dashboard/profile',{state:{username}})
   }
   return (
     <div>
         <h1>Home Page</h1>
+        <input type="text" value={username} onChange={(e)=> setUserName(e.target.value)} />
         <button onClick={handleClick}>Go to Profile Page</button>
       
     </div>
