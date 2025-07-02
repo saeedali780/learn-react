e171ac07fe9c90a02eb705eadc6f8612c0403072
Saeed Ali <saeedaliofficial77@gmail.com>
2025-07-03 02:49:16 +0500

    JSX exercise

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1>Hello World</h1>
+      <img src="https://picsum.photos/200/300" alt="" />
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <button onClick={() => alert('Hello World')}>Click me</button>
+      </ul>
+    </div>
   )
 }
 
