bf67f42175ceb032919feaf7797899c58fc7a9a2
Saeed Ali <saeedaliofficial77@gmail.com>
2025-07-04 20:27:37 +0500

    Toggle or Hide Show

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react';
 
 const App = () => {
+  const [toggle,setToggle] = useState(false);
+  const [value,setValue] = useState('');
   return (
     <div>
-      <h1>Hello World</h1>
-      <img src="https://picsum.photos/200/300" alt="" />
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <button onClick={() => alert('Hello World')}>Click me</button>
-      </ul>
+      <button onClick={() => setToggle(!toggle)}>Toggle</button>
+      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+      <button onClick={() => setValue('') }>Clear</button>
+        {toggle && value ? <h1>{value}</h1> : <h1>Please enter a value</h1>}
+     
+      
     </div>
   )
 }
