4069290f0dfe91dac118657d7b624efe8741bcd2
Saeed Ali <saeedaliofficial77@gmail.com>
2025-07-07 04:30:25 +0500

    spelling mistake

--- src/Form.jsx
+++ src/Form.jsx
@@ -13,7 +13,7 @@ const Form = () => {
         <label htmlFor="female">Female</label>
         <br/><br/>
         <h2>Selected Gender: {gender}</h2><br/>
-        <h2>Selected Gender: {city}</h2><br/>
+        <h2>Selected City: {city}</h2><br/>
         <select defaultValue={"mirpurkhas"} onChange={(e)=> setCity(e.target.value)}>
             <option value="karachi">Karachi</option>
             <option value="mirpurkhas">Mirpurkhas</option>
