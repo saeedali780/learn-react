37cfa4f7f3ab2d5c682ef253166090eb4e9ce07d
Saeed Ali <saeedaliofficial77@gmail.com>
2025-07-07 18:33:02 +0500

    styled

--- src/Persons.jsx
+++ src/Persons.jsx
@@ -3,10 +3,17 @@ import React from 'react';
 const Persons = ({user}) => {
    
   return (
-<div>
-       
-      <h1>Name: {user.name}</h1>
-      <h1>Id: {user.id}</h1>
+<div style={{
+    border:"1px solid green",
+    padding: "10px",
+    margin:"10px",
+    width: "400px",
+    borderRadius:"10px",
+}}>
+      <h2>Id: {user.id}</h2>
+      <h2>Name: {user.name}</h2>
+      <h2>Email: {user.email}</h2>
+    
 </div>
   )
 }
