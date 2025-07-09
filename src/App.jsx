befaca6e5a7af0f599184df452f0fb5b4f129541
Saeed Ali <saeedaliofficial77@gmail.com>
2025-07-10 02:29:36 +0500

    toggle with useRef hook

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,22 @@ const App = () => {
     inputRef.current.focus()
     inputRef.current.placeholder = "Hello";
     inputRef.current.value = "Hello Saeed";
+    inputRef.current.style.color = "green";
+  }
+  const toggleHandler = ()=>{
+    if(inputRef.current.style.display!= "none"){
+      inputRef.current.style.display= "none";
+    }else{
+      inputRef.current.style.display= "inline";
+    }
+  
   }
   return (
     <div>
 
       <input ref={inputRef} type="text" />
       <button onClick={handleInput}>Click Me</button>
+      <button onClick={toggleHandler}>Toggle</button>
     </div>
   )
 }
