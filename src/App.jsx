725a0c75d90b23539a2a5120601ac0742ee715f4
Saeed Ali <saeedaliofficial77@gmail.com>
2025-07-10 03:00:05 +0500

    Uncontrolled Component, Uncontrolled Component with UseRef, controlled Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,71 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 
 const App = () => {
-  const inputRef = useRef(null);
 
-  const handleInput = ()=>{
-    console.log(inputRef);
-    
-    inputRef.current.focus()
-    inputRef.current.placeholder = "Hello";
-    inputRef.current.value = "Hello Saeed";
-    inputRef.current.style.color = "green";
+  const userNameRef = useRef();
+  const userPasswordRef = useRef();
+
+
+
+
+  const [username,setUserName] = useState("");
+  const [userPassword,setUserPassword] = useState("");
+
+  const submitHandler = (e)=>{
+    e.preventDefault()
+    const user = document.querySelector("#username").value;
+    const password = document.querySelector("#password").value;
+    console.log(user,password);
+  };
+  const controledHandler = (e)=>{
+    e.preventDefault();
+    console.log(username,userPassword);
   }
-  const toggleHandler = ()=>{
-    if(inputRef.current.style.display!= "none"){
-      inputRef.current.style.display= "none";
-    }else{
-      inputRef.current.style.display= "inline";
-    }
-  
+  const submitHandlerRef = (e)=>{
+    e.preventDefault();
+    const user  = userNameRef.current.value;
+    const password = userPasswordRef.current.value;
+    console.log(user,password);
+    
+    
   }
   return (
     <div>
+      <h1>Uncontrolled Component</h1>
+      <form onSubmit={submitHandler}>
 
-      <input ref={inputRef} type="text" />
-      <button onClick={handleInput}>Click Me</button>
-      <button onClick={toggleHandler}>Toggle</button>
+        <input type="text" id='username'  placeholder='enter user name'/>
+        <br/><br/>
+        <input type="password" id='password'  placeholder='enter user password'/>
+        <br/><br/>
+        <button>
+          Submit
+        </button>
+      </form>
+      <hr/>
+
+      <h1>Uncontrolled Component with UseRef</h1>
+      <form onSubmit={submitHandlerRef}>
+        <input ref={userNameRef} type="text"   placeholder='enter user name'/>
+        <br/><br/>
+        <input ref={userPasswordRef} type="password"  placeholder='enter user password'/>
+        <br/><br/>
+        <button>
+          Submit with ref
+        </button>
+      </form>
+      <hr/>
+
+<h1>controlled Component</h1>
+<form onSubmit={controledHandler}>
+  <input type="text"  onChange={(e)=>setUserName(e.target.value)}  placeholder='enter user name'/>
+  <br/><br/>
+  <input type="password" onChange={(e)=>setUserPassword(e.target.value)}  placeholder='enter user password'/>
+  <br/><br/>
+  <button>
+    Submit
+  </button>
+</form>
     </div>
   )
 }
