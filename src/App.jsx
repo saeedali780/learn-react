ad50d75b4838a0c73a7dacaa0f8acf1c528fdf58
Saeed Ali <saeedaliofficial77@gmail.com>
2025-07-09 20:05:00 +0500

    Styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import UserCard from './UserCard';
+
+import styled from 'styled-components';
 
 const App = () => {
+  const Heading = styled.h1`
+  color:red;
+  border:1px solid green;
+  border-radious:10px;
+  margin:10px;
+  padding:10px;
+  width:200px;
+  `
   return (
-    <div>
-      <UserCard/>
+    <div style={{display:"flex", flexWrap:"wrap"}}>
+    <Heading>Hello Saeed</Heading>
+    <Heading>Hello Saeed</Heading>
+    <Heading>Hello Saeed</Heading>
+    <Heading>Hello Saeed</Heading>
+     <Heading>Hello Saeed</Heading>
+     <Heading>Hello Saeed</Heading> 
+     <Heading>Hello Saeed</Heading>
+     <Heading>Hello Saeed</Heading>
+     <Heading>Hello Saeed</Heading>
+     <Heading>Hello Saeed</Heading>
+     
+   
     </div>
   )
 }
