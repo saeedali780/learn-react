3cfc92062e440a648c4a4128fa8a10009e854047
Saeed Ali <saeedaliofficial77@gmail.com>
2025-07-10 21:10:28 +0500

    Updating Array in State

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,52 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react';
 
 const App = () => {
-  const [data,setData]= useState({
-    name:"Saeed",
-    address:{
-      city:"Karachi",
-      country:"Pakistan",
-    }
-  });
-  const handleName = (name)=>{
-    console.log(name); 
-    data.name = name
-    setData({...data})
+  const [data, setData] = useState([
+    'Alice',
+    'Bob',
+    'Charlie',
+    'Diana',
+    'Ethan'
+  ]);
+  const [dataDetails,setDataDetails] = useState([
+    { id: 1, name: 'Alice', age: 25 },
+    { id: 2, name: 'Bob', age: 28 },
+    { id: 3, name: 'Charlie', age: 22 },
+    { id: 4, name: 'Diana', age: 30 },
+    { id: 5, name: 'Ethan', age: 27 }
+  ]);
+  
+  const handleNameChange = (name)=>{
+    data[data.length -1]=name;
+    setData([...data]);
   }
-  const handleCity = (city)=>{
-    data.address.city= city
-    setData({...data,address:{...data.address,city}})
+  console.log("Before",dataDetails);
+  const handleAgeChange = (age)=>{
+    age = parseInt(age);
+    dataDetails[dataDetails.length -1].age=age;
+    setDataDetails([...dataDetails]);
+    console.log("After",dataDetails);
+    
   }
-  const handleCounty = (country)=>{
-    data.address.country= country
-    setData({...data,address:{...data.address,country}})
-  }
+  
   return (
     <div>
-      <h1>Updating objects in state</h1>
-      <h2>Name: {data.name}</h2>
-      <h2>City: {data.address.city}</h2>
-      <h2>Country: {data.address.country}</h2>
-      <input type="text" onChange={(e)=>handleName(e.target.value)} placeholder='update name'/><br/><br/>
-      <input type="text" onChange={(e)=>handleCity(e.target.value)} placeholder='update city'/><br/><br/>
-      <input type="text" onChange={(e)=>handleCounty(e.target.value)} placeholder='update country'/><br/>
+      <h1>Updating Array in State</h1>
+      <ul>
+        <h1>List of Names</h1>
+        <input type="text" onChange={(e)=>handleNameChange(e.target.value)} placeholder='enter last user name' />
+        {data.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+      <hr />
+      <h2>Data Details</h2>
+      <input type="text" onChange={(e)=>handleAgeChange(e.target.value)} placeholder='enter last user age' />
+      <ol>
+        {dataDetails.map((item) => (
+          <li key={item.id}> {item.name}, {item.age}</li>
+        ))}
+      </ol>
     </div>
   )
 }
