2ec488d4fd2d7561806180a86c61ab421140d5e1
Saeed Ali <saeedaliofficial77@gmail.com>
2025-07-11 05:14:29 +0500

    Custom Hooks in Reactjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-import React, { useState } from 'react';
-import College from './College';
-import {SubjectContext} from './ContexData.jsx';
+import React from 'react';
+import useToggle from './custom hooks/useToggle';
+
+
 
 const App = () => {
-  const [subject,setSubject] = useState("");
-
+  const [value,toggleValue]= useToggle(true)
+  console.log(value);
+  
   return (
-    <div style={{backgroundColor:"yellow", padding:10}}>
-    <SubjectContext.Provider value={subject}>
-      <select value={subject} onChange={(e)=>setSubject(e.target.value)}>
-      <option value="">Select Value</option>
+    <div>
+      {value? <h1>Custom Hooks in React</h1>  :null}
+      <button onClick={toggleValue}>Toggle Heading</button>
+      <button onClick={()=>toggleValue(false)}>Hide Heading</button>   
+      <button onClick={()=>toggleValue(true)}>Show Heading</button>   
 
-        <option value="Maths">Maths</option>
-        <option value="English">English</option>
-        <option value="Computer Science">Computer Science</option>
-      </select>
-      <h1>App</h1>
-      <button onClick={()=> setSubject("")}>Clear Subject</button>
-      <College/>
-    </SubjectContext.Provider>
     </div>
   )
 }
