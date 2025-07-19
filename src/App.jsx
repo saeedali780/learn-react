8fe5ba4c13a13ead0c7c71f2474ad7fd6892bec6
Saeed Ali <saeedaliofficial77@gmail.com>
2025-07-19 21:04:23 +0500

    Fetch and Display Data from API with

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const App = () => {
+  const [apiData,setApiData] =  useState([])
+
+  useEffect(()=>{
+    getUsersData()
+  },[])
+  const getUsersData = async ()=>{
+    const url = 'https://dummyjson.com/users';
+    let response = await fetch(url);
+   response = await response.json();
+  //  console.log(response.users);
+   
+  setApiData(response.users)
+  // console.log(apiData);
+  
+  }
+  useEffect(() => {
+    console.log(apiData);
+  }, [apiData]);
   return (
     <div>
-      <h2 className='bg-red-300 text-3xl font-bold'>Tailwind CSS Setup</h2>
+    {apiData && apiData.map((user) => (
+      <h1 key={user.id}>{user.firstName}</h1>
+    ))}
     </div>
   )
 }
