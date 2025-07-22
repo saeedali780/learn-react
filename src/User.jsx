b1c46afa45d8f6c45103b102d153c1a575d775ef
Saeed Ali <saeedaliofficial77@gmail.com>
2025-07-22 16:54:59 +0500

    Lazy Loading: Boost Performance!

---
+++ src/User.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+
+const User = () => {
+  return (
+    <div>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+        <h1>User Name is Saeed Ali</h1>
+    </div>
+  )
+}
+
+export default User
